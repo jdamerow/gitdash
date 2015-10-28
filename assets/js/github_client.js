cc355a4cabaf5ac8e3a418f986e6616b0038eb52
Julia Damerow <julia.damerow@googlemail.com>
2015-10-28 09:55:19 -0700

    made table sortable

--- assets/js/github_client.js
+++ assets/js/github_client.js
@@ -26,27 +26,50 @@ function getPullRequests(client) {
     $.ajax({
         url: client.github + "orgs/"+ client.org + "/repos",
     }).done(function(data) {
+        var data_json = []
+
         for(var i = 0; i < data.length; i++) {
             var obj = data[i];
             repositories.push(obj.name);
         }
         for (var i = 0; i < repositories.length; i++) {
+
             $.ajax({
                 url: client.github + "repos/"+ client.org + "/" + repositories[i]  + "/pulls",
+                async: false
             }).done(function(data2) {
+
                 for(var i = 0; i < data2.length; i++) {
                     var obj = data2[i];
-                    var html = '<tr>';
-                    html += '<td>' + obj.number + '</td>';
-                    html += '<td>' + obj.head.repo.name + '</td>'
-                    html += '<td><a href="' + obj.html_url + '">' + obj.title + '</a></td>';
-                    html += '<td>' + obj.user.login + '</td>';
-                    html += '<td>' + obj.created_at + '</td>';
-                    html += '</tr>';
-                    $("#pullrequests").append(html);
+                    data_json.push({number: obj.number.toString(), repository: obj.head.repo.name, title: '<a href="' + obj.html_url + '">' + obj.title + '</a>', user: obj.user.login, created: obj.created_at});
                 }
             });
         }
+        $('#pullrequests').bootstrapTable({
+            striped: true,
+            columns: [{
+                field: 'number',
+                title: 'PR#',
+                sortable: true
+            }, {
+                field: 'repository',
+                title: 'Repository',
+                sortable: true
+            }, {
+                field: 'title',
+                title: 'Title',
+                sortable: true
+            }, {
+                field: 'user',
+                title: 'User',
+                sortable: true
+            }, {
+                field: 'created',
+                title: 'Created',
+                sortable: true
+            }],
+            data: data_json
+        });
     });
 
 }
